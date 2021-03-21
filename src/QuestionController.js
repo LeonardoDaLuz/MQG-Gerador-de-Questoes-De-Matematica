85d68fbcf46075a660ce99af21223c9f9e74bacc
Leonardo a Luz <leoluzprog@gmail.com>
2021-03-21 15:42:09 -0300

    Melhorias na interface e design

--- src/QuestionController.js
+++ src/QuestionController.js
@@ -2,8 +2,7 @@ import Mathf from './Mathf';
 
 
 export default class QuestionController {
-    constructor(QuestaoData) 
-    {
+    constructor(QuestaoData) {
         this.QuestaoData = QuestaoData;
         /*
         enunciado
@@ -14,51 +13,73 @@ export default class QuestionController {
 
     gerarElementos() {
         var container = document.createElement("div");
-        container.id="Questao";
+        container.id = "Questao";
         var header = document.createElement("div");
-        header.innerHTML="Questão 1";
+        header.innerHTML = "Questão 1";
         header.classList.add("questHeader");
         container.appendChild(header);
         var enunciado = document.createElement("div");
         enunciado.classList.add("enunciado");
-        enunciado.innerHTML=this.QuestaoData.enunciado;
+        enunciado.innerHTML = this.QuestaoData.enunciado;
         container.appendChild(enunciado);
         var form = document.createElement("form");
         container.appendChild(form);
-        var alternativaCerta =  this.QuestaoData.alternativas[this.QuestaoData.alternativaCerta];
+        var alternativaCerta = this.QuestaoData.alternativas[this.QuestaoData.alternativaCerta];
         this.QuestaoData.alternativas = this.randomizeArray(this.QuestaoData.alternativas);
-        console.log("certa:"+alternativaCerta);
-        
-        for(var i=0; i<this.QuestaoData.alternativas.length; i++) {
+        console.log("certa:" + alternativaCerta);
+        var letras = ['A', 'B', 'C', 'D', 'E', 'F']
+        var elementosDasAlternativas = [];
+        for (var i = 0; i < this.QuestaoData.alternativas.length; i++) {
 
-            var alternativa = document.createElement("input");
+            let alternativa = document.createElement("input");
             alternativa.type = "radio";
-            alternativa.id = "alternativa-"+i;
-            alternativa.value =  alternativa.id ;
+            alternativa.id = "alternativa-" + i;
+            alternativa.value = alternativa.id;
             alternativa.name = "alt";
-            var label = document.createElement("label");
-            label.htmlFor =  alternativa.id;
+            let label = document.createElement("label");
+            label.htmlFor = alternativa.id;
             label.innerHTML = this.QuestaoData.alternativas[i];
             label.prepend(alternativa);
             form.appendChild(label);
+            let AltLetra = document.createElement("div");
+            AltLetra.classList.add("alternativaLetra");
+            AltLetra.innerHTML = letras[i];
+            label.prepend(AltLetra);
             //form.appendChild(document.createElement("br"));
             console.log(this.QuestaoData.alternativas[i]);
 
-            if(alternativaCerta==this.QuestaoData.alternativas[i]){
+            elementosDasAlternativas.push(label);
+
+            label.addEventListener('click', function (event) {
+                if (event.target != label)
+                    return;
+
+                seleciona(event, elementosDasAlternativas);
+            });
+
+            if (alternativaCerta == this.QuestaoData.alternativas[i]) {
                 label.classList.add("alternativaCerta");
             }
         }
 
 
-        return  container ;
+        function seleciona(event, elementosDasAlternativas) {
+            elementosDasAlternativas.forEach(element => {
+                element .classList.remove('selected');
+            })
+
+            event.target.classList.add("selected");
+        }
+
+        return container;
     }
 
     randomizeArray(array) {
         var oldArray = array;
         var length = array.length;
         var newArray = [];
-        for(var i = 0; i<length; i++){
-            var random = Math.round(Mathf.RandomRange(0, oldArray.length-1));
+        for (var i = 0; i < length; i++) {
+            var random = Math.round(Mathf.RandomRange(0, oldArray.length - 1));
             newArray.push(oldArray[random]);
             oldArray.splice(random, 1);
         }
